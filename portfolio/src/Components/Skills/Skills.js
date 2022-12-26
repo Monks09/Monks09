9f04dbcc3ca652030fe95d0d9cff949bf5a45ed6
Monks09 <mayanksharma5104@gmail.com>
2022-12-27 01:59:57 +0530

    changes

--- portfolio/src/Components/Skills/Skills.js
+++ portfolio/src/Components/Skills/Skills.js
@@ -5,15 +5,16 @@ function Skills(props) {
     return <div className="Skills">
         <h1>SKILLS</h1>
         <div className="skillsContainer">
-            <SkillCard image="https://mohitkss.github.io/static/media/html5.4e6edfe05f07c383e94f.png" skill_name="HTML" />
+            <SkillCard image="https://cdn-icons-png.flaticon.com/128/174/174854.png" skill_name="HTML" />
             <SkillCard image="https://mohitkss.github.io/static/media/css3.845e8eb63836bef093cf.png" skill_name="CSS" />
-            <SkillCard image="https://mohitkss.github.io/static/media/js.3dfb665e058d08c99f28.png" skill_name="JavaScript"/>
-            <SkillCard image="https://mohitkss.github.io/static/media/git.b1472a80b81e487179cf.png" skill_name="Git/Github" />
-            <SkillCard image="https://mohitkss.github.io/static/media/mongodb.4f7af09e6354d51beec8.png" skill_name="MongoDB" />
-            <SkillCard image="https://mohitkss.github.io/static/media/nodejs.b508473ad71a31ce2fae.png" skill_name="NodeJS" />
-            <SkillCard image="https://mohitkss.github.io/static/media/react.22d1f2096ed82cab7a8a.png" skill_name="ReactJS" />
-            <SkillCard image="https://mohitkss.github.io/static/media/redux.7fec6369cecd1cbd44d6.png" skill_name="Redux" />
-            <SkillCard image="https://mohitkss.github.io/static/media/mern.b58d81727b47466a7cda.png" skill_name="MERN" />
+            <SkillCard image="https://cdn-icons-png.flaticon.com/128/5968/5968292.png" skill_name="JavaScript" />
+            <SkillCard image="https://img.icons8.com/color/48/000000/react-native.png" skill_name="ReactJS" />
+            <SkillCard image="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" skill_name="Redux" />
+            <SkillCard image="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg" skill_name="NodeJS" />
+            <SkillCard image="http://www.kaustubh.app/img/expressjs.8f887594.svg" skill_name="ExpressJS" />
+            <SkillCard image="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg" skill_name="MongoDB" />
+            <SkillCard image="https://cdn-icons-png.flaticon.com/128/733/733609.png" skill_name="Git/Github" />
+            <SkillCard image="https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667" skill_name="Postman" />
         </div>
     </div>
 }
